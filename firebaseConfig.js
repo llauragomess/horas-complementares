73a61a61d4876954f0ad94c85fab32689825fb19
llauragomess <lauragomes18123@gmail.com>
2025-02-21 10:37:33 -0300

    Remover firebaseConfig.js do repositório e adicionar ao .gitignore

--- firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDozkS5Oe1uV85tiznKRzAE8b5Frz93ao4",
-    authDomain: "horas-complementares-d84f8.firebaseapp.com",
-    projectId: "horas-complementares-d84f8",
-    storageBucket: "horas-complementares-d84f8.firebasestorage.app",
-    messagingSenderId: "172075774197",
-    appId: "1:172075774197:web:c1b2f6d72e3faad74995bc"
-};
